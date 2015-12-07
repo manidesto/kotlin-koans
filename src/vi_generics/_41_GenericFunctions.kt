8cc1a723407dea36b3c02603db0cbed234d66781
manidesto <manideepiitb@gmail.com>
2015-12-07 21:32:29 +0530

    Solution to vi_generics

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -1,8 +1,7 @@
 package vi_generics
 
-import java.util.ArrayList
-import java.util.HashSet
 import util.TODO
+import java.util.*
 
 fun task41(): Nothing = TODO(
     """
@@ -19,13 +18,17 @@ fun task41(): Nothing = TODO(
             l.toCollection(HashSet<Int>())
         }
 )
+fun <T, E : MutableCollection<T>> Collection<T>.partitionTo(success: E, failure : E, predicate : (T) -> Boolean) : Pair<E, E>{
+    for(item : T in this) {
+        if(predicate(item)) success.add(item) else failure.add(item)
+    }
+    return Pair(success, failure)
+}
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
